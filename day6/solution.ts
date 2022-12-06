6186f26b9e47b2cda58c920d7f6c2186f8ef3a34
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-12-06 11:15:12 +0100

    day6

--- day6/solution.ts
+++ day6/solution.ts
@@ -11,24 +11,10 @@ const star1 = (lengthOfSignal : number) => {
 
     for (let i = 0; i < message.length; i++) {
         const element = message.slice(i,i+lengthOfSignal);
-        const signalCheck =  [...new Set(element) ]        
-        // console.log(signalCheck.length , i+lengthOfSignal);
-        
+        const signalCheck =  [...new Set(element) ]                
         if(signalCheck.length === lengthOfSignal) return i+lengthOfSignal;
     }
 }
-// const inputData: string[] = syncReadFile('./day6/input.txt')
-// // const message = [...inputData[0]]
-// const star1 = (lengthOfSignal : number) => {
-//     let signalCheck =  new Set()        
-//     for (let i = 0; i < message.length; i++) {
-//         const element = message[i]
-//         signalCheck.add(element);
-//         // console.log(signalCheck.size , i+lengthOfSignal);
-        
-//         if(signalCheck.size === lengthOfSignal) return i+lengthOfSignal;
-//     }
-// }
 console.log(star1(4));
 const star2 = star1(14)
 console.log(star2);
